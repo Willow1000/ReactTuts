d351cea839617c6bbaaabefb1eb4d56f010c16cc
willow <wilkinsondari7@gmail.com>
2024-07-02 08:41:00 +0300

    ADDED TOGGLE COLOR BUTTON,HEX

--- proj2/Proj2.jsx
+++ proj2/Proj2.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import colorNames from 'colornames'
 
 function Proj2(props) {
   return (
     <section>
         <article className='container' style={{
-            background:`${props.value}`
+            background:`${props.value}`,
+            color:`${props.isDark?'#fff':'#000'}`
         }}>
-            {props.value?props.value:'Empty Value'}
+            <span>{props.value?props.value:'Empty Value'}</span>
+            <span>{props.hexValue?props.hexValue:null}</span>
         </article> 
-        <input type="text" autoFocus className='input' onInput={(e)=>props.setValue(e.target.value)} placeholder='Add Color Name'/>
+        <input type="text" autoFocus className='input' onChange={(e)=>{props.setValue(e.target.value)
+            props.setHexValue(colorNames(e.target.value))
+        } }placeholder='Add Color Name'/>
+        <button onClick={()=>{props.setIsDark(!props.isDark)}}>Toggle Text Color</button>
     </section> 
   )
 }
 
+Proj2.defaultProps={
+    value:'Empty Color Value'
+}
 export default Proj2
